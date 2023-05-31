06a383b92753aab94ce0725fcafca34e69b29fd3
“AndreaFerreiro” <andreafga28@gmail.com>
2023-05-31 19:35:09 +0200

    do some improves

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const select= document.querySelector('.js_select');
 const bet = document.querySelector ('.js_bet');
 const buttom = document.querySelector ('.js_buttom');
+const reset =document.querySelector ('.js_reset');
 const result = document.querySelector ('.js_result');
 const balance = document.querySelector ('.js_balance');
 let saldo = 100;
@@ -21,25 +22,25 @@ function Bet(){
     const selectValue = parseInt(select.value);
     const randomNumber = getRandomNumber(6);
     console.log (randomNumber);
-    console.log (selectValue);
+    if (selectValue === randomNumber){
+        result.innerHTML = 'Has ganado el doble de lo apostado!';
+        win();
+        balance.innerHTML= saldo;
+        buttom.classList.add('hidden');
+        reset.classList.remove('hidden');
+    }else{
+        result.innerHTML = 'Has perdido lo apostado!';
+        fail();
+        balance.innerHTML= saldo;
+        buttom.classList.add('hidden');
+        reset.classList.remove('hidden');
+    }
     if (saldo <= 0){
         result.innerHTML = '¡Que pena! Has perdido.';
         buttom.setAttribute("disabled", "disabled");
     }else if (saldo >= 200){
         result.innerHTML = '¡Felicidades! Has ganado!';
         buttom.setAttribute("disabled", "disabled");
-    } else if (betValue > saldo){
-        result.innerHTML = 'No tienes saldo suficiente para esa apuesta';
-    } else if (selectValue === randomNumber){
-        result.innerHTML = 'Has ganado el doble de lo apostado!';
-        win();
-        balance.innerHTML= saldo;
-        console.log (saldo);
-    } else{
-        result.innerHTML = 'Has perdido lo apostado!';
-        fail();
-        balance.innerHTML= saldo;
-        console.log (saldo);
     }
 }
 function handleEvent (event) {
@@ -47,3 +48,7 @@ function handleEvent (event) {
     Bet();
 }
 buttom.addEventListener ('click', handleEvent);
+function handleReset (event){
+    document.location.reload();
+}
+reset.addEventListener('click',handleReset);

b9f57ad249ad049b70bee5694ac7e53a1d74539b
“AndreaFerreiro” <andreafga28@gmail.com>
2023-06-12 21:52:07 +0200

    resolve issues

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const reset =document.querySelector ('.js_reset');
 const result = document.querySelector ('.js_result');
 const balance = document.querySelector ('.js_balance');
 let saldo = 100;
-const betValue= bet.value;
 function win() {
     const betValue= bet.value;
     saldo = saldo+betValue*2;
@@ -18,28 +17,30 @@ function fail() {
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+function innerResult(message){
+    result.innerHTML = message;
+}
 function Bet(){
     const selectValue = parseInt(select.value);
     const randomNumber = getRandomNumber(6);
-    console.log (randomNumber);
     if (selectValue === randomNumber){
-        result.innerHTML = 'Has ganado el doble de lo apostado!';
+        innerResult('Has ganado el doble de lo apostado!');
         win();
         balance.innerHTML= saldo;
         buttom.classList.add('hidden');
         reset.classList.remove('hidden');
     }else{
-        result.innerHTML = 'Has perdido lo apostado!';
+        innerResult('Has perdido lo apostado!');
         fail();
         balance.innerHTML= saldo;
         buttom.classList.add('hidden');
         reset.classList.remove('hidden');
     }
     if (saldo <= 0){
-        result.innerHTML = '¡Que pena! Has perdido.';
+        innerResult('¡Que pena! Has perdido.');
         buttom.setAttribute("disabled", "disabled");
     }else if (saldo >= 200){
-        result.innerHTML = '¡Felicidades! Has ganado!';
+        innerResult('¡Felicidades! Has ganado!');
         buttom.setAttribute("disabled", "disabled");
     }
 }
